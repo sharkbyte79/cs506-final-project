de7a4c37ca9e73e20e85a8947cb25b10b8d62852
Pard3sh <pard3sh@bu.edu>
2025-03-31 12:52:17 -0400

    beginning work on visualization -- added functions for column summary display

--- src/cs506_final_project/visualization.py
+++ src/cs506_final_project/visualization.py
@@ -5,6 +5,31 @@ PROJECT_ROOT = Path(__file__).resolve().parents[2]
 TARGET_FILE = "mini_ICE_data.csv"
 FILEPATH = f"{PROJECT_ROOT}/data/raw/{TARGET_FILE}"
 
-dataframe = csv_to_df(FILEPATH)
-print(dataframe)
 
+def plot_bar_count(df, column_name):
+    pass
+
+def plot_time_series(df, date_column):
+    pass
+
+def plot_heatmap(df, groupby_col):
+    pass
+
+def plot_distribution(df, column_name):
+    pass
+
+def print_column_stats(df, column_name):
+    print(f"Stats for {column_name}")
+    print(df[column_name].describe())
+
+def print_all_stats(df):
+    for col in df:
+        print_column_stats(df, col)
+
+def main():
+    dataframe = csv_to_df(FILEPATH)
+    print("Dataframe: ", dataframe)
+    print_all_stats(dataframe)
+
+if __name__ == "__main__":
+    main()
