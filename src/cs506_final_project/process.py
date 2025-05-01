d1b8d67cb179ef53b8409fe7558ce815515ae299
Layne <pierce77@bu.edu>
2025-05-01 12:06:30 -0400

    Preprocessing functions for converting AOR to latitude and longitude via OpenStreetMap

--- src/cs506_final_project/process.py
+++ src/cs506_final_project/process.py
@@ -1,10 +1,13 @@
-from pandas import DataFrame, read_csv, to_datetime, concat
+from pandas import DataFrame, read_csv, to_datetime, concat, Series
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.ensemble import RandomForestClassifier
+import time
+from geopy.geocoders import Nominatim
 
 
 # wrapper functions for data manipulation and preprocessing
+
 
 def csv_to_df(fpath: str) -> DataFrame:
     """Returns the csv specified by fpath as a DataFrame with basic processing"""
@@ -16,7 +19,13 @@ def csv_to_df(fpath: str) -> DataFrame:
     )  # Convert rows where AOR is 'Unmapped' to NaN
 
     # shorten long column names
-    df = df.rename(columns={"Area of Responsibility (AOR)": "AOR", "Country of Citizenship": "Citizenship", "Administrative Arrests": "Arrests"})
+    df = df.rename(
+        columns={
+            "Area of Responsibility (AOR)": "AOR",
+            "Country of Citizenship": "Citizenship",
+            "Administrative Arrests": "Arrests",
+        }
+    )
 
     # Convert 'Month-Year features to datetime objects for sorting
     df["Month-Year"] = to_datetime(df["Month-Year"], format="%b %Y")
@@ -32,18 +41,40 @@ def csv_to_df(fpath: str) -> DataFrame:
 
     return df
 
-def encode_and_scale(train_df: DataFrame, test_df: DataFrame, id: str, target: str, categorial: list[str]):
+
+def aor_to_longitude(df: DataFrame) -> DataFrame:
+    """Returns a version of the given DataFrame df with AOR converted to latitude and longitude features"""
+    df = df.dropna(subset=['AOR']) 
+    geolocator = Nominatim(user_agent='pierce77@bu.edu') # must pass email to avoid being blocked by openstreetmap api
+    df[["Latitude", "Longitude"]] = df["AOR"].apply(lambda aor: get_latlong(aor, geolocator))
+    return df.drop("AOR", axis=1)
+
+
+def get_latlong(aor: str, geolocator) -> Series:
+    """Gets the latitude and longitude for an AOR"""
+    time.sleep(1)  # sleep for a second to respect rate limits
+    loc = geolocator.geocode(aor.lower())
+    return Series([loc.latitude, loc.longitude]) 
+
+
+def encode_and_scale(
+    train_df: DataFrame, test_df: DataFrame, id: str, target: str, categorial: list[str]
+):
     """Returns a version of the given DataFrame df with label encoding and standard scaling applied"""
 
-    numerical: list[str] = [col for col in train_df.columns if col not in [id, target, *categorial]]
+    numerical: list[str] = [
+        col for col in train_df.columns if col not in [id, target, *categorial]
+    ]
     for col in categorial:
-        encoder: LabelEncoder = LabelEncoder() # Create encoder for each iteration
-        combined: DataFrame = concat([train_df[col], train_df[col]]) # Combine the columns for consistent label encoding
+        encoder: LabelEncoder = LabelEncoder()  # Create encoder for each iteration
+        combined: DataFrame = concat(
+            [train_df[col], train_df[col]]
+        )  # Combine the columns for consistent label encoding
         encoder.fit(combined)
 
         train_df[col] = encoder.transform(train_df[col])
         test_df[col] = encoder.transform(test_df[col])
-    
+
     # Using standard scaling on each numerical column
     # Every value is considered equally important when scaled with mean + stdev
     scaler: StandardScaler = StandardScaler()
@@ -52,15 +83,11 @@ def encode_and_scale(train_df: DataFrame, test_df: DataFrame, id: str, target: s
 
     # TODO: define X_train, Y_train, etc for model training
 
-    
 
 def split_data(df: DataFrame) -> tuple[DataFrame, DataFrame]:
     """Returns a split of the given data as train and test sets"""
-    train, test = train_test_split(
-        df, random_state=42, test_size=0.3
-    )  
+    train, test = train_test_split(df, random_state=42, test_size=0.3)
 
     map((DataFrame), train, test)
 
     return train, test
-
